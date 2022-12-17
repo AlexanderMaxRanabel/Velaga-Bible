77e10890c0a6f7475eed82d41396ff88ec78437f
AlexanderMaxRanabel <93342430+AlexanderMaxRanabel@users.noreply.github.com>
2022-12-17 19:23:16 +0300

    Update lands.cljs

--- lands.cljs
+++ lands.cljs
@@ -17,3 +17,31 @@ And it came to pass that the Lord spoke unto his people, saying, 'Verily I say u
 "Let the words of my mouth and the meditation of my heart be acceptable in your sight, O Lord, my rock and my redeemer. For you are my fortress and my shield, and in you I trust."
 
 "Love one another as I have loved you, for in this is my commandment. By this all people will know that you are my disciples, if you have love for one another."
+
+    "Do unto others as you would have them do unto you, for this is the law and the prophets. Love your neighbor as yourself, and you will fulfill all righteousness."
+
+    "The Lord is my shepherd, I shall not want. He makes me lie down in green pastures and leads me beside still waters. He restores my soul and leads me in paths of righteousness for his name's sake."
+
+    "I will not leave you as orphans, I will come to you. In the world you will have tribulation, but take heart, for I have overcome the world."
+
+    "For God so loved the world, that he gave his only begotten Son, that whoever believes in him should not perish but have eternal life. For God did not send his Son into the world to condemn the world, but in order that the world might be saved through him."
+
+    "Let the peace of Christ rule in your hearts, for as members of one body you are called to peace. And be thankful."
+
+    "The Lord is my rock and my fortress, my deliverer; my God, my rock, in whom I take refuge, my shield, and the horn of my salvation, my stronghold."
+
+    "I will give you a new heart and put a new spirit within you; I will take the heart of stone out of your flesh and give you a heart of flesh. And I will put my Spirit within you and cause you to walk in my statutes and be careful to obey my rules."
+
+    "Let your light shine before others, that they may see your good works and glorify your Father in heaven."
+
+    "But seek first the kingdom of God and his righteousness, and all these things will be added to you."
+
+    "For I know the plans I have for you, declares the Lord, plans for welfare and not for evil, to give you a future and a hope."
+
+    "But you are a chosen race, a royal priesthood, a holy nation, a people for his own possession, that you may proclaim the excellencies of him who called you out of darkness into his marvelous light."
+
+    "For there is one God, and there is one mediator between God and men, the man Christ Jesus, who gave himself as a ransom for all, which is the testimony given at the proper time."
+
+    "The Spirit himself bears witness with our spirit that we are children of God, and if children, then heirs—heirs of God and fellow heirs with Christ, provided we suffer with him in order that we may also be glorified with him."
+
+    "For the grace of God has appeared, bringing salvation for all people, training us to renounce ungodliness and worldly passions,
