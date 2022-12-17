b093b53700e5c654b2721886bb9b8a04999e91f4
AlexanderMaxRanabel <93342430+AlexanderMaxRanabel@users.noreply.github.com>
2022-12-17 19:13:23 +0300

    Update lands.cljs

--- lands.cljs
+++ lands.cljs
@@ -1,1 +1,19 @@
 And it came to pass that the Lord spoke unto his people, saying, 'Verily I say unto you, ye shall prosper and flourish in all that ye do, for I am with you always. For I am the light in the darkness, the hope in the despair, and the joy in the sorrow. Trust in me and I will guide you on the path to righteousness and bring you to the land of milk and honey.' And the people rejoiced, for they knew that the Lord was with them."
+"Blessed are the meek, for they shall inherit the earth. And blessed are the peacemakers, for they shall be called the children of God."
+"Fear not, for I am with you. I will uphold you with my righteous right hand, and I will keep you safe from all harm. Trust in me, and you shall not be afraid."
+
+"As the mountains surround Jerusalem, so I surround you with my love and protection. I am your refuge and your strength, and I will never leave you nor forsake you."
+
+"Do not let your hearts be troubled or afraid. Believe in me and all that I have promised, and you will have life eternal in my kingdom."
+
+"Let your light shine before others, so that they may see your good works and give glory to your Father in heaven. For I am the light of the world, and in me there is no darkness."
+
+"Humble yourselves before me, and I will lift you up. Seek me with all your heart, and you will find me. For I am near to all who call upon me in truth."
+
+"Be strong and courageous, for I am with you wherever you go. I will guide you and protect you, and I will bring you to the land that I have promised you."
+
+"Rejoice always, for I am with you and my joy is in you. Let your heart be filled with gratitude and thanksgiving, for all that I have done for you."
+
+"Let the words of my mouth and the meditation of my heart be acceptable in your sight, O Lord, my rock and my redeemer. For you are my fortress and my shield, and in you I trust."
+
+"Love one another as I have loved you, for in this is my commandment. By this all people will know that you are my disciples, if you have love for one another."
