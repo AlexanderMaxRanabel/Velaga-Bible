14161df798ffd14988b7a45abc8fc7f3963bece3
AlexanderMaxRanabel <93342430+AlexanderMaxRanabel@users.noreply.github.com>
2023-02-05 23:18:32 +0300

    Create suffer.cpp

---
+++ suffer.cpp
@@ -0,0 +1,2 @@
+Solvebunt terras arenae. sicut populus meus post haec sustinebit. 
+fugiat nulla sit. homines nonnisi dolere pergunt. mortales esse intellegent neque aliud quam immortale patitur. non est simplex effugium
