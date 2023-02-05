c296aa36b7409234181a85a6d8ff57cc72f26afa
AlexanderMaxRanabel <93342430+AlexanderMaxRanabel@users.noreply.github.com>
2023-02-05 23:20:27 +0300

    Create suffer.c

---
+++ suffer.c
@@ -0,0 +1,13 @@
+Solvebunt terras arenae. sicut populus meus post haec sustinebit. 
+  fugiat nulla sit. 
+  homines nonnisi dolere pergunt. 
+  mortales esse intellegent neque aliud quam immortale patitur. 
+  non est simplex effugium
+
+nomen eius voco. Sine fine patet
+Illa mihi respondebo
+ubi est maybe
+immortalis she
+revertere ad me
+
+Ego Geniunly ne sicut ipse
